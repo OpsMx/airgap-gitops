c868f369084a7fdc69e9aba3ee32a93e92875729
sagayd <sagayaraj.d@opsmx.io>
2021-08-22 21:45:23 +0530

    Aug 23 scripts for NAB

---
+++ install-time-scripts/aug23/download-from-s3.sh
@@ -0,0 +1,13 @@
+mkdir -p spin-images; cd spin-images
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/mc.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/forwarder-controller.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/forwarder-agent.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/ubi8-gate.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/ubi8-oes-dashboard.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/ubi8-oes-db.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/ubi8-oes-platform.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/ubi8-oes-sapor.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/ubi8-oes-ui.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/ubi8-oes-autopilot.tar
+wget https://airgapped-repo.s3.ap-southeast-2.amazonaws.com/spin-images/ubi8-oes-visibility.tar
+cd ..
