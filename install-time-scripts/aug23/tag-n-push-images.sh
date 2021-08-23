6b2e257a8ec2aa45eb248e9a93818e63cf6560d1
sagayd <sagayaraj.d@opsmx.io>
2021-08-23 11:42:23 +0530

    docker tag added to the command

--- install-time-scripts/aug23/tag-n-push-images.sh
+++ install-time-scripts/aug23/tag-n-push-images.sh
@@ -1,14 +1,14 @@
 # Docker Tag
-quay.io/opsmxpublic/ubi8-gate:v3.9.1.2  PRIV-REGISTRY/ubi8-gate:v3.9.1.2
-quay.io/opsmxpublic/ubi8-oes-autopilot:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-autopilot:v3.9.1.2
-quay.io/opsmxpublic/ubi8-oes-dashboard:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-dashboard:v3.9.1.2
-quay.io/opsmxpublic/ubi8-oes-platform:v3.9.1.3  PRIV-REGISTRY/ubi8-oes-platform:v3.9.1.3
-quay.io/opsmxpublic/ubi8-oes-sapor:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-sapor:v3.9.1.2
-quay.io/opsmxpublic/ubi8-oes-ui:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-ui:v3.9.1.2
-quay.io/opsmxpublic/ubi8-oes-visibility:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-visibility:v3.9.1.2
-quay.io/opsmxpublic/forwarder-controller:v20210722T133815  PRIV-REGISTRY/forwarder-controller:v20210722T133815
-quay.io/opsmxpublic/forwarder-agent:v20210722T133815  PRIV-REGISTRY/forwarder-agent:v20210722T133815
-minio/mc:RELEASE.2020-11-25T23-04-07Z  PRIV-REGISTRY/minio/mc:RELEASE.2020-11-25T23-04-07Z
+docker tag quay.io/opsmxpublic/ubi8-gate:v3.9.1.2  PRIV-REGISTRY/ubi8-gate:v3.9.1.2
+docker tag quay.io/opsmxpublic/ubi8-oes-autopilot:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-autopilot:v3.9.1.2
+docker tag quay.io/opsmxpublic/ubi8-oes-dashboard:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-dashboard:v3.9.1.2
+docker tag quay.io/opsmxpublic/ubi8-oes-platform:v3.9.1.3  PRIV-REGISTRY/ubi8-oes-platform:v3.9.1.3
+docker tag quay.io/opsmxpublic/ubi8-oes-sapor:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-sapor:v3.9.1.2
+docker tag quay.io/opsmxpublic/ubi8-oes-ui:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-ui:v3.9.1.2
+docker tag quay.io/opsmxpublic/ubi8-oes-visibility:v3.9.1.2  PRIV-REGISTRY/ubi8-oes-visibility:v3.9.1.2
+docker tag quay.io/opsmxpublic/forwarder-controller:v20210722T133815  PRIV-REGISTRY/forwarder-controller:v20210722T133815
+docker tag quay.io/opsmxpublic/forwarder-agent:v20210722T133815  PRIV-REGISTRY/forwarder-agent:v20210722T133815
+docker tag minio/mc:RELEASE.2020-11-25T23-04-07Z  PRIV-REGISTRY/minio/mc:RELEASE.2020-11-25T23-04-07Z
 
 
 # Docker Push
